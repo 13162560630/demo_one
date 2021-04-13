1cb1dc395db4f49b4eb25b50c1d729384fff6f39
13162560630 <13162560630@163.com>
2021-04-13 10:58:33 +0800

    修改main()方法，增加一行代码567。

--- demo_three/src/main/java/com/example/DemoThreeApplication.java
+++ demo_three/src/main/java/com/example/DemoThreeApplication.java
@@ -9,6 +9,7 @@ public class DemoThreeApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoThreeApplication.class, args);
         System.out.println("demo_three内容1234");
+        System.out.println("demo_three内容567");
         
     }
 
