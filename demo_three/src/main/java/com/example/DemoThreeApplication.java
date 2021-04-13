ee3747b205b5a83397618b9b879d9ce8f743999c
13162560630 <65879039+13162560630@users.noreply.github.com>
2021-04-13 10:49:22 +0800

    Update DemoThreeApplication.java

--- demo_three/src/main/java/com/example/DemoThreeApplication.java
+++ demo_three/src/main/java/com/example/DemoThreeApplication.java
@@ -8,11 +8,7 @@ public class DemoThreeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoThreeApplication.class, args);
-        System.out.println("demo_three内容");
-        System.out.println("demo_three内容");
-        System.out.println("demo_three内容2");
-        System.out.println("demo_three内容3");
-        System.out.println("demo_three内容4");
+        System.out.println("demo_three内容1234");
         
     }
 
